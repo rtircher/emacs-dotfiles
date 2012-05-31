f17c2808f2fdf8d814060b9c511b38031c787f00
Renaud Tircher <rtircher@thoughtworks.com>
2012-05-30 22:22:17 -0700

    Adding hook for clojure-test-mode to match file naming conventions of current project

--- .emacs.d/rtircher-clojure.el
+++ .emacs.d/rtircher-clojure.el
@@ -2,4 +2,12 @@
           (lambda ()
             (setq inferior-lisp-program "/usr/local/bin/lein repl"))) ;; This is some crap to make it work in MacOSX since it looks like emacs doesn't find my PATH
 
+(defun rtr-clojure-test-maybe-enable nil
+  "Enable clojure-test-mode if the current buffer contains \"clojure.test\" package."
+  (let ((ns (clojure-find-package)))
+    (when (search "-test" ns)
+      (save-window-excursion (clojure-test-mode t)))))
+
+(add-hook 'clojure-mode-hook 'rtr-clojure-test-maybe-enable)
+
 (provide 'rtircher-clojure)
