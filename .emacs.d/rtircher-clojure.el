7c0d2f257a3abfe78b5752f67caf4451293f549a
Renaud Tircher <renaud.tircher@gmail.com>
2012-10-17 23:14:46 -0700

    Adding compojure indentation rules

--- .emacs.d/rtircher-clojure.el
+++ .emacs.d/rtircher-clojure.el
@@ -10,4 +10,16 @@
 
 (add-hook 'clojure-mode-hook 'rtr-clojure-test-maybe-enable)
 
+(require 'clojure-mode)
+
+(define-clojure-indent
+  (defroutes 'defun)
+  (GET 2)
+  (POST 2)
+  (PUT 2)
+  (DELETE 2)
+  (HEAD 2)
+  (ANY 2)
+  (context 2))
+
 (provide 'rtircher-clojure)
