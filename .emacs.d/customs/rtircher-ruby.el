9415d78df0d08b4ccc2f630e9248d36fc4707e24
Renaud Tircher <rtircher@teslamotors.com>
2014-08-08 08:18:50 -0700

    Syntax sugar for guardfiles

--- .emacs.d/customs/rtircher-ruby.el
+++ .emacs.d/customs/rtircher-ruby.el
@@ -4,7 +4,9 @@
 ;; Use ruby mode for Gemfiles
 (add-to-list 'auto-mode-alist '("[Gg]emfile\\'" . ruby-mode))
 ;; Use ruby mode for Vagrantfiles
-(add-to-list 'auto-mode-alist '("[Va]agrantfile\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("[Vv]agrantfile\\'" . ruby-mode))
+;; Use ruby mode for Guardfiles
+(add-to-list 'auto-mode-alist '("[Gg]uardfile\\'" . ruby-mode))
 ;; Use ruby mode for prawn files
 (add-to-list 'auto-mode-alist '("\\.prawn\\'" . ruby-mode))
 
